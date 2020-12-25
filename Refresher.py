5d86e4d7278b15c93a35394f0bc0781382db072b
VedantGawande <73682466+VedantGawande@users.noreply.github.com>
2020-12-25 14:46:42 +0530

    Update Refresher.py

--- Refresher.py
+++ Refresher.py
@@ -1,9 +1,12 @@
 import pyautogui - #pip install pyautogui
+# There is a default value of user_input to use in function# I put the refreshing part in a for loop of the range of user input(to Refresh as many times user said)
 user_input = 0
+
 def user():
+    # I have made a function that has a global variabel user_input which takes input.
     global user_input
     user_input = int(input("How many times do you want to refresh? "))
-
+# It takes the input again if the user inputs a string or aything else with the help of a while loop.
 try:
     user()
     wrong = False
@@ -13,17 +16,17 @@ except:
     while wrong == False:
         user()
 
-# Setting a limit
+# I have also set a limit so that if user inputs large number it won't keep refreshing (you can change it)
 while user_input > 25:
     print("Please enter a Number below 25".)
     user()
 
+# With the help of pyautogui I pressed
 # Start + D - Dextop
 pyautogui.keyDown('win')
 pyautogui.keyDown('d')
 pyautogui.keyUp('win')
 pyautogui.keyUp('d')
-
 
 
 
@@ -34,7 +37,8 @@ for v in range(user_input):
     pyautogui.keyDown('f10')
     pyautogui.keyUp('f10')
     pyautogui.keyUp('shift')
-
+    
+    # To reach refresh button and press enter
     # Down arrow three times
     pyautogui.press('down', presses=3)
 
